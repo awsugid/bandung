25be69ba512350ede96bf90fbd0bd69da266eea2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2026-05-09 23:02:51 +0700

    feat: add meetup event data

---
+++ src/lib/events.ts
@@ -0,0 +1,52 @@
+export const meetupGroupUrl = "https://www.meetup.com/aws-user-group-bandung/";
+
+export const events = [
+	{
+		id: "314688796",
+		title: "AWS User Group Bandung Meetup May 2026",
+		type: "Meetup",
+		statusLabel: "Upcoming",
+		dateLabel: "Saturday, May 16, 2026",
+		timeLabel: "4:00 PM - 6:00 PM WIB",
+		location: "MIRE HUB - Creative Hub & Event Space",
+		address:
+			"Jl. Brigadir Jend. Katamso No.19B, Cihaur Geulis, Kec. Cibeunying Kaler, Kota Bandung, Jawa Barat 40122",
+		city: "Bandung",
+		priceLabel: "Free",
+		attendeeCount: 2,
+		host: "Arsy Opraza Akma",
+		hostDisplayName: "Arsy Opraza A.",
+		groupName: "AWS User Group Bandung",
+		groupRating: "5.0",
+		groupReviewCount: 8,
+		imageUrl:
+			"https://secure.meetupstatic.com/photos/event/5/2/5/7/highres_534141079.webp?w=3840",
+		meetupUrl:
+			"https://www.meetup.com/aws-user-group-bandung/events/314688796/?eventOrigin=group_featured_event",
+		mapUrl:
+			"https://www.google.com/maps/search/?api=1&query=-6.904756%2C%20107.631134",
+		coordinates: {
+			latitude: -6.904756,
+			longitude: 107.631134,
+		},
+		speakers: [
+			{
+				name: "Fauzi Kurniawan",
+				role: "Code Reviewer & Mentor at Dicoding Indonesia",
+				topic:
+					"Beyond Just Face Detection: Exploring AWS Rekognition Capabilities.",
+			},
+		],
+		topics: ["AI/ML", "Amazon Web Services", "Cloud Computing"],
+		description:
+			"Join AWS User Group Bandung for a May meetup on AWS Rekognition, real-world cloud learning, and professional networking with fellow IT practitioners in Bandung.",
+		details: [
+			"Join us at the AWS User Group Bandung Meetup May 2026.",
+			"Learn, share experiences, and expand your professional network with fellow IT practitioners in Bandung.",
+			"The speaker will share insights, best practices, and real-world examples for both seasoned AWS users and people who are just starting out.",
+			"Use this session to expand your knowledge of cloud technologies and connect with like-minded professionals.",
+		],
+	},
+];
+
+export const featuredEvent = events[0];
