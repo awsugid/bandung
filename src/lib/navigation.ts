3ed8178b765f9f0369dc8ca412cb7ddf45361b76
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2026-05-09 20:58:35 +0700

    refactor: simplify navigation utilities

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -1,8 +1,5 @@
 export const navItems = [
-    { name: 'Home', href: '/' },
-    { name: 'Events', href: '/events' },
-    { name: 'Blog', href: '/blog' },
-    { name: 'Volunteer', href: '/volunteer' },
-    { name: 'Speakers', href: '/speakers' },
-    { name: 'Sponsors', href: '/sponsor' },
+	{ name: "Home", href: "/" },
+	{ name: "About", href: "#about" },
+	{ name: "Start", href: "#start" },
 ];
