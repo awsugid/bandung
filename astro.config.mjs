2122132a49df82ecb52b01515c87b05ec4a93f8e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2026-05-09 20:57:48 +0700

    chore: simplify project config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
-import mdx from "@astrojs/mdx";
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
-
   vite: {
     plugins: [tailwindcss()],
     server: {
